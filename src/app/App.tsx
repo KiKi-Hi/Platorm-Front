921d0f11b90ebe72ef64ae0c49e6170be087c63f
chloe_choi <plumxeun@gmail.com>
2025-04-11 20:38:08 +0900

    setting-0: package setting

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <h1 className="text-2xl font-bold">키키하이 시작!</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    entry: './src/main.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        historyApiFallback: true,
+        port: 3000,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
